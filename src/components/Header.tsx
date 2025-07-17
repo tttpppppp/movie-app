e9b0f19e476c70a6e7105d9569ce77b09a32c14f
Trần Tiến Phúc <112532197+tttpppppp@users.noreply.github.com>
2025-07-17 07:43:26 +0700

    initial commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,12 +9,16 @@ const Header = () => {
       <header className="h-14 bg-slate-950 text-white flex items-center justify-between p-6">
         <div className="flex items-center gap-4">
           <Link to="/">
-            <img src="/netflix.png" className="w-16 sm:w-28 cursor-pointer" />
+            <img
+              src="/netflix.png"
+              className="w-16 sm:w-28 cursor-pointer"
+              alt="logo"
+            />
           </Link>
           <Link to={"/search?mediatype=movie"}>Phim</Link>
           <Link to={"/search?mediatype=tv"}>Truyền Hình</Link>
         </div>
-        <Link to={"/search"}>
+        <Link to={"/search"} aria-label="Tìm kiếm">
           <FontAwesomeIcon
             icon={faMagnifyingGlass}
             className="cursor-pointer"

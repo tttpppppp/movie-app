a1c1c1c429b14987e1844b80cefade3464bec3e1
Trần Tiến Phúc <112532197+tttpppppp@users.noreply.github.com>
2025-07-17 19:44:08 +0700

    initial commit

--- src/components/MediaDedtail/RelatedMediaList.tsx
+++ src/components/MediaDedtail/RelatedMediaList.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
 import type { MediaType } from "../../types/shared-media.type";
 import MovieCard from "../MediaList/MovieCard";
 interface Props {
@@ -6,12 +8,34 @@ interface Props {
   title: string;
 }
 const RelatedMediaList = ({ mediaList = [], title }: Props) => {
+  const [visibleCount, setVisibleCount] = useState(8);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleScroll = () => {
+    const scrollBottom =
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 100;
+
+    if (scrollBottom && !isLoading && visibleCount < mediaList.length) {
+      setIsLoading(true);
+      setTimeout(() => {
+        setVisibleCount((prev) => Math.min(prev + 8, mediaList.length));
+        setIsLoading(false);
+      }, 500);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const visibleItems = mediaList.slice(0, visibleCount);
   return (
     <div>
       <p className="font-bold text-[1.4vw] my-4">{title}</p>
       {mediaList.length > 0 ? (
         <div className="grid grid-cols-3 sm:grid-cols-4 gap-4">
-          {mediaList.map((item: MediaType) => (
+          {visibleItems.map((item: MediaType) => (
             <MovieCard
               key={item.id}
               id={item.id}
@@ -27,6 +51,11 @@ const RelatedMediaList = ({ mediaList = [], title }: Props) => {
         </div>
       ) : (
         <p className="text-center w-full">Đang cập nhật...</p>
+      )}
+      {isLoading && (
+        <div className="flex justify-center items-center mt-4">
+          <div className="w-8 h-8 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+        </div>
       )}
     </div>
   );
